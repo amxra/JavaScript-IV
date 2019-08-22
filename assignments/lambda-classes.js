3a87cd804f7416d6d7ed2838444a4594f1d69e90
amxra <amiradediran@gmail.com>
2019-08-22 13:48:47 +0100

    Create person class in lambda-classes.js

--- assignments/lambda-classes.js
+++ assignments/lambda-classes.js
@@ -1,1 +1,19 @@
 // CODE here for your Lambda Classes
+
+
+class Person {
+    constructor(name, location, age){
+        this.name  =name;
+        this.location = location;
+        this.age =age;
+    }
+    speak (){
+        console.log (`Hello my name is ${this.name}, I am from ${this.location} `)
+    }
+}
+
+// const amira = new Person ('amira','lagos', 12)
+
+// console.log(amira.speak());
+
+
